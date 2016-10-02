34159f052e9f3bc6231cf1efb5a0e6e48e79f8be
georgebv <georgeme007@gmail.com>
2016-10-01 22:20:02 -0400

    update

--- coastlib/coreutils/design_tools.py
+++ coastlib/coreutils/design_tools.py
@@ -1,7 +1,7 @@
-from numpy import tanh, tan, exp
+from numpy import tan, exp
 from math import pi
 import scipy.constants
-from scipy.optimize import newton
+
 
 g = scipy.constants.g  # gravity constant (m/s^2) as defined by ISO 80000-3
 sea_water_density = 1025  # sea water density (kg/m^3)
