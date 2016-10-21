ec7d28913199a4e15b69de67175cb9418d0fcf8e
George Bocharov <georgeme007@gmail.com>
2016-10-21 09:12:32 -0400

    threading

--- test/test.py
+++ test/test.py
@@ -27,18 +27,30 @@ class ProgressBar(threading.Thread):
             print_update('! {0} {1} [100%]'.format(self.text, bar))
 
 
+class MainThread(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        # This is the dummy work block
+        global progress1
+        for i in range(0, 100):
+            progress1 += 1
+            time.sleep(0.1)
+            if progress1 == 50:
+                print('\n\nSome extra work can be executed here! (btw its 50%!)\n')
+
 
 stdoutmutex = threading.Lock()
 progress1 = 0
 threads = []
-thread = ProgressBar(mutex=stdoutmutex, text='Printing i\'s')
-thread.start()
-threads.append(thread)
-
-for i in range(0, 100):
-    progress1 += 10
-    time.sleep(0.1)
+thread1 = ProgressBar(mutex=stdoutmutex, text='Printing i\'s')
+thread2 = MainThread()
+thread1.start()
+thread2.start()
+threads.append(thread1)
+threads.append(thread2)
 
 for thread in threads:
     thread.join()
-print('Over')
+print('\n\nAll work is done! Ready to close all threads!')
