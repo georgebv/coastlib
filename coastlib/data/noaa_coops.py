b2087e28a07293762553ed5701c4a180a218d79c
georgebv <georgeme007@gmail.com>
2019-08-02 18:39:30 -0400

    NOAA CO-OPS API Batch

--- coastlib/data/noaa_coops.py
+++ coastlib/data/noaa_coops.py
@@ -2,6 +2,8 @@ from datetime import datetime
 from urllib.request import urlopen
 import json
 
+from coastlib.helper.progress_bar import ProgressBar
+
 import numpy as np
 import pandas as pd
 
@@ -21,7 +23,7 @@ def nanfloat(v):
 def coops_api(station, begin_date, end_date, product, datum, **kwargs):
     """
     Parses NOAA Tides & Currents web data via the NOAA CO-OPS data retrieval API.
-    See https://tidesandcurrents.noaa.gov/api/
+    See https://tidesandcurrents.noaa.gov/api/ for reference.
 
     Parameters
     ----------
@@ -35,6 +37,10 @@ def coops_api(station, begin_date, end_date, product, datum, **kwargs):
     end_date : str
         End date-time.
         yyyyMMdd, yyyyMMdd HH:mm, MM/dd/yyyy, or MM/dd/yyyy HH:mm.
+        Maximum retrieval time:
+            31 days     All 6 minute data products
+            1 year      Hourly Height, and High/Low
+            10 years    Tide Predictions, Daily, and Monthly Means
     product : str
         water_level             Preliminary or verified water levels, depending on availability.
         air_temperature         Air temperature as measured at the station.
@@ -101,8 +107,10 @@ def coops_api(station, begin_date, end_date, product, datum, **kwargs):
     Examples
     --------
     >>> df = coops_api(station=8518750, begin_date='20121025', end_date='20121101', product='water_level', datum='NAVD')
-    >>> df['v'].max()
-    11.28
+    >>> df.columns.to_list()
+    ['v', 's', 'f', 'q']
+    >>> df['v'].mean()
+    1.5010661458333334
     """
 
     units = kwargs.pop('units', 'english')
@@ -191,6 +199,101 @@ def coops_api(station, begin_date, end_date, product, datum, **kwargs):
         raise RuntimeError
 
 
+def coops_api_batch(begin_date, end_date, return_logs=False, echo_progress=False, **kwargs):
+    """
+    Expands functionality of the <coops_api> function allowing to extract data for
+    arbitrary time periods.
+
+    Parameters
+    ----------
+    begin_date : datetime.datetime or str
+        Start date-time - must be either a datetime object or a string convertible to datetime object.
+    end_date : datetime.datetime or str
+        End date-time - must be either a datetime object or a string convertible to datetime object.
+    return_logs : bool, optional
+        If True, returns a dictionary with logs for all iterations.
+    echo_progress : bool, optional
+        If True, prints out progress bar (default=False).
+    kwargs
+        See <coops_api> docstring for a full list of arguments.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        Pandas DataFrame with parsed time series.
+
+    Examples
+    --------
+    >>> df = coops_api_batch(station=8518750, begin_date='20120825', end_date='20121101', product='water_level', datum='NAVD')
+    >>> df.columns.to_list()
+    ['v', 's', 'f', 'q']
+    >>> df['v'].mean()
+    0.29498921634703756
+    """
+
+    product = kwargs['product']
+    if product in ['daily_mean', 'monthly_mean']:
+        time_delta = pd.to_timedelta('3500D')
+    elif product in ['hourly_height', 'high_low']:
+        time_delta = pd.to_timedelta('350D')
+    else:
+        time_delta = pd.to_timedelta('30D')
+
+    if not isinstance(begin_date, datetime):
+        begin_date = pd.to_datetime(begin_date)
+
+    if not isinstance(end_date, datetime):
+        end_date = pd.to_datetime(end_date)
+
+    logs = {}
+    progress_bar = ProgressBar(
+        total_iterations=int(np.ceil((end_date - begin_date) / time_delta)),
+        bars=50, bar_items='0123456789#', prefix='NOAA CO-OPS'
+    )
+    if echo_progress:
+        print(progress_bar, end='\r')
+
+    data = []
+    _start = begin_date
+    _end = begin_date + time_delta
+    while _end - time_delta <= end_date:
+        try:
+            data.append(coops_api(begin_date=_start.strftime('%Y%m%d'), end_date=_end.strftime('%Y%m%d'), **kwargs))
+            logs[len(logs)] = {
+                'start': begin_date,
+                'end': end_date,
+                'success': True,
+                'error_message': None
+            }
+        except ValueError as _err:
+            logs[len(logs)] = {
+                'start': begin_date,
+                'end': end_date,
+                'success': False,
+                'error_message': str(_err)
+            }
+        _start += time_delta
+        _end += time_delta
+        if echo_progress:
+            progress_bar.increment()
+            print(progress_bar, end='\r')
+
+    if len(data) > 0:
+        df = pd.concat(data)
+        df.sort_index(inplace=True)
+        df = df[~df.index.duplicated(keep='first')]
+        df = df[(df.index >= begin_date) & (df.index <= end_date)]
+    else:
+        df = None
+    if echo_progress:
+        print(progress_bar)
+
+    if return_logs:
+        return df, logs
+
+    return df
+
+
 def coops_datum(station, units='metric', metadata=False):
     """
     Parses NOAA Tides & Currents tidal datums for given station via the NOAA CO-OPS data retrieval API.
@@ -270,5 +373,9 @@ def coops_datum(station, units='metric', metadata=False):
 
 
 if __name__ == "__main__":
-    coops_api_data = coops_api(station=8518750, begin_date='20121025', end_date='20121101', product='water_level')
-    coops_api_data.plot()
+    _df, _logs = coops_api_batch(
+        station=8518750, begin_date='18120125', end_date='18121101', product='water_level',
+        datum='NAVD', echo_progress=True, return_logs=True
+    )
+    # coops_api_data = coops_api(station=8518750, begin_date='20121025', end_date='20121101', product='water_level')
+    # coops_api_data.plot()
---
+++ coastlib/helper/progress_bar.py
@@ -0,0 +1,113 @@
+import time
+
+
+def to_ds(value):
+    if len(f'{value:.0f}') < 2:
+        return f'0{value:.0f}'
+    else:
+        return f'{value:.0f}'
+
+
+class ProgressBar:
+
+    def __init__(self, total_iterations, bars=50, bar_items='0123456789#', prefix=''):
+        """
+        Progress bar.
+
+        Parameters
+        ----------
+        total_iterations : int
+            Number of iterations.
+        bars : int, optional
+            Length of progress bar (default=50).
+        bar_items : str, optional
+            String with characters used to represent intermediate states of bar. Last symbol represents a complete bar.
+        prefix : str, optional
+            Prefix placed before the progress bar (default='').
+        """
+
+        self.total_iterations = total_iterations
+        self.bars = bars
+        self.bar_items = bar_items
+        self.prefix = prefix
+
+        self.__start_time = time.time()
+        self.__percent_per_bar = 100 / self.bars
+        self.__i = 0
+        self.progress_bar = f'{prefix}    [' + ' ' * self.bars + ']'
+
+    @property
+    def i(self):
+        return self.__i
+
+    @i.setter
+    def i(self, value):
+        if value > self.total_iterations:
+            raise ValueError(f'Value {value} exceeds the iterator limit of {self.total_iterations}')
+        if not isinstance(value, int) or value < 0:
+            raise ValueError(f'{value} is not a valid iterator value')
+        self.__i = value
+        self.__update_bar()
+
+    def __repr__(self):
+        return self.progress_bar
+
+    def increment(self, inc=1):
+        """
+        Increment progress bar counter by <i>. Updates <.progress_bar> attribute.
+
+        Parameters
+        ----------
+        inc : int, optional
+            Increment value (default=1).
+        """
+
+        self.i += inc
+
+    def __update_bar(self):
+
+        percentage = self.i / self.total_iterations * 100
+        full_bars = int(percentage / self.__percent_per_bar)
+        bar = '[' + f'{self.bar_items[-1]}' * full_bars
+        partial_bar_ind = int(
+            percentage % self.__percent_per_bar / self.__percent_per_bar * (len(self.bar_items) - 1)
+        )
+        if len(bar) < (self.bars + 1):
+            bar += self.bar_items[partial_bar_ind]
+        bar += ' ' * (self.bars - (len(bar) - 1)) + ']'
+
+        elapsed = time.time() - self.__start_time
+        elapsed_string = f'{to_ds(elapsed // 3600)}:' \
+            f'{to_ds(elapsed % 3600 // 60)}:' \
+            f'{to_ds(elapsed % 60)}.{str(elapsed % 1)[2]}'
+        try:
+            speed = self.i / elapsed
+            eta = (self.total_iterations - self.i) / speed
+            eta_string = f'{to_ds(eta // 3600)}:' \
+                f'{to_ds(eta % 3600 // 60)}:' \
+                f'{to_ds(eta % 60)}.{str(eta % 1)[2]}'
+        except ZeroDivisionError:
+            speed = 0
+            eta_string = f'00:00:00.00'
+
+        self.progress_bar = str(
+            f'{self.prefix} {percentage:>3.0f}% '
+            f'{bar} '
+            f'{self.i:>{len(str(self.total_iterations))}}/{self.total_iterations:>{len(str(self.total_iterations))}} '
+            f'[ETA: {eta_string}, Elapsed: {elapsed_string}, Speed: {speed:.2f} it/s]'
+        )
+
+    def print(self):
+        """
+        Print progress bar and reset carriage to line start.
+        """
+
+        print(self.progress_bar, end='\r')
+
+
+if __name__ == '__main__':
+    self = ProgressBar(1000, prefix='Test')
+    for _ in range(1000):
+        time.sleep(0.025)
+        self.increment(1)
+        print(self, end='\r')
