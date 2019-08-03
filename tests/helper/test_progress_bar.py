971d2fab27a0219b6a3fd7b12dfb9ad239df4498
georgebv <georgeme007@gmail.com>
2019-08-02 20:40:04 -0400

    progress bar test fix

--- tests/helper/test_progress_bar.py
+++ tests/helper/test_progress_bar.py
@@ -10,39 +10,36 @@ def test_progress_bar_defaults():
     n_iter = 97
     intermediate = 30
     pb = ProgressBar(total_iterations=n_iter)
-    assert str(pb) == '   0% [0                                                 ] ' \
-                      ' 0/97 [ETA: 00:00:00.00, Elapsed: 00:00:00.0, Speed: 0.00 it/s]'
+    assert str(pb)[:71] == '   0% [0                                                 ]  0/97 [ETA: '
     for _ in range(intermediate):
         pb.increment()
-    assert str(pb)[:64] == '  31% [###############4                                  ] 30/97'
+    assert str(pb)[:71] == '  31% [###############4                                  ] 30/97 [ETA: '
     for _ in range(n_iter - intermediate):
         pb.increment()
-    assert str(pb)[:64] == ' 100% [##################################################] 97/97'
+    assert str(pb)[:71] == ' 100% [##################################################] 97/97 [ETA: '
 
 
 def test_progress_bar_customized():
     n_iter = 146
     intermediate = 121
     pb = ProgressBar(total_iterations=n_iter, bars=50, bar_items='0123456789#', prefix='HelloPB')
-    assert str(pb) == 'HelloPB   0% [0                                                 ] ' \
-                      '  0/146 [ETA: 00:00:00.00, Elapsed: 00:00:00.0, Speed: 0.00 it/s]'
+    assert str(pb)[:80] == 'HelloPB   0% [0                                                 ]   0/146 [ETA: '
     for _ in range(intermediate):
         pb.increment()
-    assert str(pb)[:73] == 'HelloPB  83% [#########################################4        ] 121/146'
+    assert str(pb)[:80] == 'HelloPB  83% [#########################################4        ] 121/146 [ETA: '
     for _ in range(n_iter - intermediate):
         pb.increment()
-    assert str(pb)[:73] == 'HelloPB 100% [##################################################] 146/146'
+    assert str(pb)[:80] == 'HelloPB 100% [##################################################] 146/146 [ETA: '
 
 
 def test_progress_bar_iprop():
     n_iter = 97
     intermediate = 30
     pb = ProgressBar(total_iterations=n_iter)
-    assert str(pb) == '   0% [0                                                 ] ' \
-                      ' 0/97 [ETA: 00:00:00.00, Elapsed: 00:00:00.0, Speed: 0.00 it/s]'
+    assert str(pb)[:71] == '   0% [0                                                 ]  0/97 [ETA: '
     for _ in range(intermediate):
         pb.i += 1
-    assert str(pb)[:64] == '  31% [###############4                                  ] 30/97'
+    assert str(pb)[:71] == '  31% [###############4                                  ] 30/97 [ETA: '
     for _ in range(n_iter - intermediate):
         pb.i += 1
-    assert str(pb)[:64] == ' 100% [##################################################] 97/97'
+    assert str(pb)[:71] == ' 100% [##################################################] 97/97 [ETA: '
