f55bcab77ce6c41a965548fc6b48e1987c630e46
georgebv <georgeme007@gmail.com>
2019-08-11 13:11:40 -0400

    license update

--- coastlib/math/vector.py
+++
@@ -1,2 +0,0 @@
-# Define Vector2 and Vector3 classes (2D and 3D accordingly)
-# Should include methods for: add, multiply, subtract, rotate, plot, get angle with another vector
--- coastlib/waves/support.py
+++ coastlib/waves/support.py
@@ -1,3 +1,19 @@
+# coastlib, a coastal engineering Python library
+# Copyright (C), 2019 Georgii Bocharov
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import coastlib.waves
 import os
 import numpy as np
@@ -5,8 +21,6 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import scipy.constants
 import scipy.optimize
-
-from coastlib.plotting.styles import coastlib_rc
 
 
 def solve_dispersion_relation(wave_period, depth, g=scipy.constants.g):
